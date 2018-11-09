ca13992e30eef8ba5af8f91d05a2c01289e0a81c
Johnny Packard <packard81@me.com>
2018-11-08 21:37:23 -0600

    adding view to header

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+// Index.ios.js - place code in here for IOS!!!!
+
 // Import a library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
-
+import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-    <Text>Some Text</Text>
+    <Header />
 );
 
 // Render it to the device
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,27 @@
 // import libraries for making a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, View } from 'react-native';
 
 // make a component
 const Header = () => {
-    return <Text></Text>;
-}
-// make the component available to other parts of the app
+    const { textStyle, viewStyle } = styles;
 
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>  
+        </View>
+    );     
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
+
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+// make the component available to other parts of the app
+export default Header;

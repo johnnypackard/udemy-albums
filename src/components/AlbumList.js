ba24f5b482bfd1b8a012af59fc6b4863bd2cf951
Johnny Packard <packard81@me.com>
2018-11-09 10:26:07 -0600

    passing props to album detail and list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    // map over the albums and get title name 
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
     
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album List!</Text>
+                {this.renderAlbums()}
             </View>
         );    
     }

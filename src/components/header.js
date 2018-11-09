b2a43cd406853ac2e3219e9eb351cfaaee573c0b
Johnny Packard <packard81@me.com>
2018-11-08 21:11:30 -0600

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+// import libraries for making a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// make a component
+const Header = () => {
+    return <Text></Text>;
+}
+// make the component available to other parts of the app
+
